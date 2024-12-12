7b79f1f6ce02fbfcdb24050bb6680a4b25d40d52
Bruno-MP-Student <bruno.martinpena@estaca.eu>
2024-12-12 18:29:00 +0100

    Initial commit second try

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(a: int, b: int) -> int:
+    """
+    Adds two numbers.
+
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    """
+    Subtracts the second number from the first.
+
+    :param a: First number
+    :param b: Second number
+    :return: Difference between a and b
+    """
+    return a - b
